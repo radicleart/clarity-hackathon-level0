64772660c50fb6482671a7284e4a2ca468d82207
Mike Cohen <mjoecohen@gmail.com>
2020-05-27 11:41:49 +0100

    Added few more unit tests to the project map.

--- contracts/projects.clar
+++ contracts/projects.clar
@@ -1,14 +1,15 @@
 ;; Project Administration
-;; --------------------------
+;; ----------------------
 
 ;; Constants
+;; ---------
 (define-constant administrator 'ST18PE05NG1YN7X6VX9SN40NZYP7B6NQY6C96ZFRC)
 (define-constant not-allowed (err 1))
+(define-constant not-found (err 2))
 
 ;; Storage
-(define-data-var counter int 0)
-
-;; PROJECT-MAP
+;; -------
+;; project-map
 ;; params: 
 ;;     base-url - url from where to read project meta data
 ;;     mint-fee - fee 
@@ -19,19 +20,41 @@
 ;; Add a new project - administrator level call.
 (define-public (add-project (projectId principal) (baseUrl (buff 40)) (mintFee uint))
   (begin
-    (if (is-allowed)
+    (if (is-create-allowed)
+      (begin
+        (map-insert project-map {project-id: projectId} ((base-url baseUrl) (mint-fee mintFee)))
+        (ok projectId)
+      )
+      (err not-allowed)
+    )
+  )
+)
+
+;; Update new project - project owner level call.
+(define-public (update-project (projectId principal) (baseUrl (buff 40)) (mintFee uint))
+  (begin
+    (if (is-update-allowed projectId)
       (begin
         (map-set project-map {project-id: projectId} ((base-url baseUrl) (mint-fee mintFee)))
-        (ok projectId))
+        (ok projectId)
+      )
       (err not-allowed)
     )
   )
 )
 
 (define-read-only (get-project (projectId principal))
-  (unwrap-panic (map-get? project-map {project-id: projectId})))
+  (match (map-get? project-map {project-id: projectId})
+    myProject (ok myProject) (err not-found)  
+  )
+)
 
 ;; Only contract administrator can do these things.
-(define-private (is-allowed)
+(define-private (is-create-allowed)
   (is-eq tx-sender administrator)
+)
+
+;; Only contract administrator can do these things.
+(define-private (is-update-allowed (projectId principal))
+  (is-eq tx-sender projectId)
 )
--- test/projects.ts
+++ test/projects.ts
@@ -5,12 +5,14 @@ import * as fs from "fs";
 describe("projects tutorial test suite", () => {
 
   const contractKeys = JSON.parse(fs.readFileSync("./keys-contract.json").toString());
-  const project1Keys = JSON.parse(fs.readFileSync("./keys-minter.json").toString());
+  const project1Keys = JSON.parse(fs.readFileSync("./keys-project1.json").toString());
+  const project2Keys = JSON.parse(fs.readFileSync("./keys-project2.json").toString());
   let client: Client;
   let provider: Provider;
 
   const contractKey = contractKeys.stacksAddress;
   const project1Key = project1Keys.stacksAddress;
+  const project2Key = project2Keys.stacksAddress;
 
   before(async () => {
     provider = await ProviderRegistry.createProvider();
@@ -25,23 +27,50 @@ describe("projects tutorial test suite", () => {
   });
 
   describe("Project map tests", () => {
-    it("should allow new project if tx-sender is contract owner", async () => {
-      let txreceive = await execMethod(client, contractKey, "add-project", [ `'${project1Key}`, "\"http://risido.com/assets/v1\"","u5000"]);
-      assert.isOk(txreceive.success, "Added project");
+    it("should allow insert if tx-sender is contract owner", async () => {
+      let txreceive = await execMethod(client, contractKey, "add-project", [ `'${project1Key}`, "\"http://project1.com/assets/v1\"","u5000"]);
+      assert.isOk(txreceive.success, "Transaction succeeded");
       const result = await readFromContract(client, "get-project", [`'${project1Key}`]);
-      assert.equal(result.strings[0], "http://risido.com/assets/v1");
+      assert.isOk(result.rawResult.indexOf('u5000') > -1, "Ensure mint price is correct");
+      assert.equal(result.strings[0], "http://project1.com/assets/v1");
       assert.equal(result.strings.length, 1);
     })
-    it("should allow new project if tx-sender is contract owner", async () => {
-      let txreceive = await execMethod(client, contractKey, "add-project", [ `'${project1Key}`, "\"http://risido.com/assets/v1\"","u5000"]);
-      assert.isOk(txreceive.success, "Added project");
+    it("should return error if no project found", async () => {
+      const result = await readFromContract(client, "get-project", [`'${project2Key}`]);
+      assert.isOk(result.rawResult.indexOf('err 2') > -1, "returns error: not found");
+      assert.equal(result.strings.length, 0);
+    })
+    it("should prevent update if project already exists", async () => {
+      let txreceive = await execMethod(client, contractKey, "add-project", [ `'${project1Key}`, "\"http://apples.com/assets/v1\"","u7000"]);
+      assert.isOk(txreceive.success, "Transaction succeeded");
       const result = await readFromContract(client, "get-project", [`'${project1Key}`]);
-      assert.equal(result.strings[0], "http://risido.com/assets/v1");
+      assert.isOk(result.rawResult.indexOf('u7000') === -1, "Ensure mint price has not been changed");
+      assert.equal(result.strings[0], "http://project1.com/assets/v1");
+      assert.equal(result.strings.length, 1);
+    })
+    it("should prevent insert when tx signer not contract owner", async () => {
+      let txreceive = await execMethod(client, project1Key, "add-project", [ `'${project1Key}`, "\"http://apples.com/assets/v1\"","u7000"]);
+      assert.isNotOk(txreceive.success, "Transaction failed");
+    })
+    it("should allow insert of new project", async () => {
+      let txreceive = await execMethod(client, contractKey, "add-project", [ `'${project2Key}`, "\"http://project2.com/assets/v1\"","u10000"]);
+      assert.isOk(txreceive.success, "Transaction failed");
+      const result = await readFromContract(client, "get-project", [`'${project2Key}`]);
+      assert.isOk(result.rawResult.indexOf('u10000') > -1, "Ensure mint price is correct");
+      assert.equal(result.strings[0], "http://project2.com/assets/v1");
+      assert.equal(result.strings.length, 1);
+    })
+    it("should allow update of existing project only if tx signer is project owner", async () => {
+      let txreceive = await execMethod(client, project2Key, "update-project", [ `'${project2Key}`, "\"http://project2.com/assets/v2\"","u8000"]);
+      assert.isOk(txreceive.success, "Transaction failed");
+      const result = await readFromContract(client, "get-project", [`'${project2Key}`]);
+      assert.isOk(result.rawResult.indexOf('u8000') > -1, "Ensure mint price is correct");
+      assert.equal(result.strings[0], "http://project2.com/assets/v2");
       assert.equal(result.strings.length, 1);
     })
   });
   
-after(async () => {
+  after(async () => {
     await provider.close();
   });
 });
